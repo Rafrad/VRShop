126ac12e007fb4662a86ff14f8fe072f254821cf
wiktor <wiktor.nocon@kolomololabs.com>
2020-06-02 22:53:10 +0200

    added basic script

---
+++ Assets/Scripts/CustomGrabbable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGrabbable : OVRGrabbable
+{
+    private Product product;
+
+    override protected void Start()
+    {
+        base.Start();
+        product = GetComponent<Product>();
+    }
+    override public void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        product.Grab();
+    }
+    override public void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        product.GrabEnd();
+    }
+}
